25255de9119241114248c76990eaec72075d9b16
GvYoutube <119984713+Gvyt@users.noreply.github.com>
2025-07-29 08:57:09 -0500

    Create nut-button.js

---
+++ wtv/nut-button.js
@@ -0,0 +1,37 @@
+class NutButton extends HTMLElement {
+  constructor() {
+    super();
+    const shadowRoot = this.attachShadow({ mode: 'open' });
+
+    
+    shadowRoot.innerHTML = `
+      <style>
+        button {
+          padding: 10px 20px;
+          font-size: 16px;
+          cursor: pointer;
+          background-color: #4CAF50; /* Green */
+          color: white;
+          border: none;
+          border-radius: 5px;
+        }
+        button:hover {
+          background-color: #45a049;
+        }
+      </style>
+      <button id="myButton">
+        <slot></slot> </button>
+      <audio id="nut" src="nut.wav"></audio> `;
+  }
+
+  connectedCallback() {
+    const button = this.shadowRoot.getElementById('myButton');
+    const audio = this.shadowRoot.getElementById('myAudio');
+
+    button.addEventListener('click', () => {
+      audio.play();
+    });
+  }
+
+
+customElements.define('nut', NutButton);
